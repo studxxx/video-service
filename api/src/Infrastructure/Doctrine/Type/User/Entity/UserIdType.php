13d3af51a9142eec0592487529fbdf1a9addc72c
studxxx <stud181177@gmail.com>
2020-03-15 18:53:04 +0200

    Moved types to other folder

--- api/src/Infrastructure/Doctrine/Type/User/Entity/UserIdType.php
+++ api/src/Infrastructure/Doctrine/Type/User/Entity/UserIdType.php
@@ -1,15 +1,15 @@
 <?php
 declare(strict_types=1);
 
-namespace Api\Infrastructure\Doctrine\Type\User;
+namespace Api\Infrastructure\Doctrine\Type\User\Entity;
 
-use Api\Model\User\Entity\User\Email;
+use Api\Model\User\Entity\User\UserId;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\GuidType;
 
-class EmailType extends StringType
+class UserIdType extends GuidType
 {
-    public const NAME = 'user_user_email';
+    public const NAME = 'user_user_id';
 
     public function getName(): string
     {
@@ -18,11 +18,11 @@ class EmailType extends StringType
 
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
-        return $value instanceof Email ? $value->getEmail() : $value;
+        return $value instanceof UserId ? $value->getId() : $value;
     }
 
     public function convertToPHPValue($value, AbstractPlatform $platform)
     {
-        return !empty($value) ? new Email($value) : null;
+        return !empty($value) ? new UserId($value) : null;
     }
 }
