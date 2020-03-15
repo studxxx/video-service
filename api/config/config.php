9871a1a6e93a5b051a447ab1918cac4524f9a544
studxxx <stud181177@gmail.com>
2020-03-15 11:22:45 +0200

    Added Doctrine libs and config

--- api/config/config.php
+++ api/config/config.php
@@ -2,6 +2,11 @@
 declare(strict_types=1);
 
 use Api\Http\Action;
+use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+use Psr\Container\ContainerInterface;
 
 return [
     'settings' => [
@@ -12,4 +17,26 @@ return [
     Action\HomeAction::class => function () {
         return new Action\HomeAction();
     },
+
+    EntityManagerInterface::class => function (ContainerInterface $container) {
+        $params = $container['config']['doctrine'];
+        $config = Setup::createAnnotationMetadataConfiguration(
+            $params['metadata_dirs'],
+            $params['dev_mode'],
+            $params['cache_dir'],
+            new FilesystemCache($params['cache_dir']),
+            false
+        );
+
+        return EntityManager::create($params['connection'], $config);
+    },
+
+    'config' => [
+        'doctrine' => [
+            'dev_mode' => true,
+            'cache_dir' => 'var/cache/doctrine',
+            'metadata_dirs' => ['src/Model/User/Entity'],
+            'connection' => ['url' => getenv('API_DB_URL')]
+        ]
+    ],
 ];
