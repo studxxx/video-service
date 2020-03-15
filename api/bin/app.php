bb47abe35c648a5e1e7bb51501070541ec050ee2
studxxx <stud181177@gmail.com>
2020-03-15 11:59:09 +0200

    Added doctrine commands to app cli list

--- api/bin/app.php
+++ api/bin/app.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
@@ -16,6 +19,11 @@ if (file_exists('.env')) {
 $container = require 'config/container.php';
 
 $cli = new Application('Application console');
+
+$entityManager = $container->get(EntityManagerInterface::class);
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+
+ConsoleRunner::addCommands($cli);
 
 $commands = $container->get('config')['console']['commands'];
 
