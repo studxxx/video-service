899c41797bb9ac1c73eff40b9f19e51bf6237cd9
studxxx <stud181177@gmail.com>
2020-05-27 23:47:18 +0300

    Added Author model

---
+++ api/src/Model/Video/UseCase/Author/Create/Command.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Api\Model\Video\UseCase\Author\Create;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $name;
+}
