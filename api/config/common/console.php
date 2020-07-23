bc18d3fd43460dd628be2afd7ad20dea6b8a9f6c
studxxx <stud181177@gmail.com>
2020-07-23 09:19:46 +0300

    Configured demo kafka

--- api/config/common/console.php
+++ api/config/common/console.php
@@ -1,12 +1,26 @@
 <?php
 declare(strict_types=1);
 
+use Api\Console\Command;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
 return [
+    Command\Amqp\ConsumeCommand::class => function (ContainerInterface $container) {
+        $brokers = $container->get('config')['console']['brokers'];
+        return new Command\Amqp\ConsumeCommand($container->get(LoggerInterface::class), $brokers);
+    },
+    Command\Amqp\ProduceCommand::class => function (ContainerInterface $container) {
+        $brokers = $container->get('config')['console']['brokers'];
+        return new Command\Amqp\ProduceCommand($container->get(LoggerInterface::class), $brokers);
+    },
     'config' => [
         'console' => [
             'commands' => [
-
+                Command\Amqp\ConsumeCommand::class,
+                Command\Amqp\ProduceCommand::class,
             ],
+            'brokers' => 'kafka:9092'
         ],
     ],
 ];
